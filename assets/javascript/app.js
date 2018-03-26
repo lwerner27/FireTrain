b99cda711a140dd970ac3ada847513ca8ac3b5e3
lwerner27 <lwerner27@gmail.com>
2018-03-26 13:15:32 -0500

    Tracker updates and train additions functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-var moment = require('moment');
-moment().format();
-
 var config = {
     apiKey: "AIzaSyBOoGzSsTAw5WmwuNSVBpzhAEl8TRaopY4",
     authDomain: "firetrain-b1351.firebaseapp.com",
@@ -14,3 +11,70 @@ var config = {
 
   let database = firebase.database()
 
+  let trainsArr = []
+  
+  let newTrain;
+  let newDest;
+  let newFreq;
+  let newStart;
+
+  let trainName = $("#train-name")
+  let trainDest = $("#train-dest")
+  let trainStart = $("#train-start")
+  let trainFreq = $("#train-freq")
+
+  let trainTable = $("#train-table")
+
+  let submitBtn = $("#submit-button")
+
+
+database.ref().on("value", function(snapshot) {
+
+  trainsArr = snapshot.val().trains
+
+  for (let i = 0; i < trainsArr.length; i++) {
+    let currentTrain = trainsArr[i]
+
+    let newTR = $("<tr>")
+    let nameData = $("<td>").text(currentTrain.name)
+    let destData = $("<td>").text(currentTrain.dest)
+    let startData = $("<td>").text(currentTrain.start)
+    let freqData = $("<td>").text(currentTrain.freq)
+
+    newTR.append(nameData, destData, startData, newFreq)
+    
+    trainTable.append(newTR)
+    
+
+  }
+})
+
+submitBtn.on("click", function () {
+
+  newTrain = trainName.val()
+  newDest = trainDest.val()
+  newStart = trainStart.val()
+  newFreq = trainFreq.val()
+  
+
+  trainName.val("")
+  trainDest.val("")
+  trainStart.val("")
+  trainFreq.val("")
+
+  let newTrainObj = {
+    name: newTrain,
+    dest: newDest,
+    start: newStart,
+    freq: newFreq
+  }
+
+
+  trainsArr.push(newTrainObj)
+
+  database.ref().set({
+    trains: trainsArr,
+})
+
+
+})
